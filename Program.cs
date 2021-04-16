2cc4b71dc5e432b262c34f5d0724335639ba59f8
jalenzero26 <jcarino062601@gmail.com>
2021-04-16 20:00:31 +0800

    initial commit - faculty class

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Faculty_Class_Management_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faculty Class Management System \n");
+            Console.WriteLine("Please enter your credentials \n");
+            string usern;
+            string passw;
+
+            Console.WriteLine("Enter Username:");
+            usern = Console.ReadLine();
+            Console.WriteLine("Enter Password:");
+            passw = Console.ReadLine();
+            if  (usern == "admin" && passw == "1234")
+            {
+                Console.WriteLine("Account Verified\n ");
+                Console.WriteLine("Welcome " + usern + '\n');
+            } 
+            else
+            {
+                Console.WriteLine("Login Failed");
+
+            }
+
+        }
+    }
+}
